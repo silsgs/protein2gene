ad7b7ad98672161f25b5e65f6bb3943aab386fbc
silsgs <silvia.santam@gmail.com>
2017-09-21 14:46:44 +0200

    Firsts changes, tabs and name/main completed!

--- proteinfilter.py
+++ proteinfilter.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 import sys
 
 #Definitions
@@ -8,32 +12,34 @@ output_filename = sample_filename.replace('.txt', '_filterprot.txt')
 
 
 def select_proteins(filename):
-	f = open(filename, 'r')
-	proteins = {}
-	for line in f:
-		line_elements = line.split()
-		if len(line_elements)==1:
-			proteins[line_elements[0]] = -1
-		else: 
-			proteins[line_elements[0]] = line_elements[1]
-	f.close()
-	return proteins
-
-sample_proteins = select_proteins(sample_filename)
-control_proteins = select_proteins(control_filename)
+    f = open(filename, 'r')
+    proteins = {}
+    for line in f:
+        line_elements = line.split()
+        if len(line_elements)==1:
+            proteins[line_elements[0]] = -1
+        else: 
+            proteins[line_elements[0]] = line_elements[1]
+    f.close()
+    return proteins
 
 
-output_file = open(output_filename, 'w')
-for p in sample_proteins:
-	if sample_proteins[p] < 0:
-		if p not in control_proteins:
-			output_file.write(p + '\n')
-	else:
-		if p in control_proteins:
-			if float(sample_proteins[p]) - float(control_proteins[p]) >= AIcutoff:
-				output_file.write(p + '\n')
-		else:
-			if float(sample_proteins[p]) >= AIcutoff:
-				output_file.write(p + '\n')
+if __name__ == "__main__":
+    
+    sample_proteins = select_proteins(sample_filename)
+    control_proteins = select_proteins(control_filename)
+
+    output_file = open(output_filename, 'w')
+    for p in sample_proteins:
+        if sample_proteins[p] < 0:
+            if p not in control_proteins:
+                output_file.write(p + '\n')
+        else:
+            if p in control_proteins:
+                if float(sample_proteins[p]) - float(control_proteins[p]) >= AIcutoff:
+                    output_file.write(p + '\n')
+            else:
+                if float(sample_proteins[p]) >= AIcutoff:
+                    output_file.write(p + '\n')
 
 output_file.close()
